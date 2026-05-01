71c5ee11a3ae13ddb75d801cdb9149694b3a9068
znotfireman <znotfireman@gmail.com>
2026-05-01 15:11:40 +0700

    fix: github & instagram links

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ export namespace Links {
     export const CREEPY_INSTAGRAM = "https://www.instagram.com/artsycreepy/"
 
     export const FIRE_PRONOUNS = "https://en.pronouns.page/@godmothersfire"
-    export const FIRE_INSTAGRAM = "https://instagram.com/@znotfireman"
-    export const FIRE_GITHUB = "https://github.com/@znotfireman"
+    export const FIRE_INSTAGRAM = "https://instagram.com/znotfireman"
+    export const FIRE_GITHUB = "https://github.com/znotfireman"
     export const FIRE_ROBLOX = "https://www.roblox.com/users/10014656431/profile"
     export const FIRE_LUDUVO = "https://alpha.luduvo.com/profile/155"
     export const FIRE_YOUTUBE = "https://www.youtube.com/@godmothersfire"
