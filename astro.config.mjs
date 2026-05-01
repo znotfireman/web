0b28e231ba4387430ddbe8ee7bf62e1910e89b88
znotfireman <znotfireman@gmail.com>
2026-05-01 15:05:56 +0700

    ci: first attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://znotfireman.github.io',
+
   vite: {
     plugins: [tailwindcss()]
   },
